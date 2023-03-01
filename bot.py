54b3bf9af490999c62fb0a551b5e2ab16d98ddbe
thegrungler <c.justliketheword@gmail.com>
2023-02-28 20:41:34 -0700

    initial commit

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+from discord import app_commands
+import os
+import random
+from dotenv import load_dotenv
+
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.all()
+client = discord.Client(command_prefix='!', intents=intents)
+tree = app_commands.CommandTree(client)
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+  print("syncing commands")
+  await tree.sync()
+  print("commands are Ready!")
+
+client.run(DISCORD_TOKEN)
