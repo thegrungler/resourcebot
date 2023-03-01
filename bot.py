d5f69022d3f2d95f19f5b3d3f6b6d13cce5c710b
thegrungler <c.justliketheword@gmail.com>
2023-02-28 20:54:16 -0700

    added drop table template

--- bot.py
+++ bot.py
@@ -11,10 +11,24 @@ intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
 tree = app_commands.CommandTree(client)
 
-@tree.command(name="ping", description="test command")
-async def ping(interaction)
-  interaction.response.send_message("pong")
+resourceTables = {}
 
+#Prints a given dictionary
+def dictString(dict):
+  output = ""
+  for key, value in dict.items():
+    output = output + (key + ": " + str(value) + "\n")
+  return output
+
+@tree.command(name="ping", description="test command")
+async def ping(interaction):
+  await interaction.response.send_message("pong")
+
+@tree.command(name="template", description="template for pulling resources")
+async def template(interaction):
+  embedVar = discord.Embed(title="Drop tables", color= random.randint(0x000000, 0xffffff)) #Chooses a random hexadecimal value for a color
+  embedVar.add_field(name= "Locations: Rate", value=dictString({"resource": "location"}), inline=False)
+  await interaction.response.send_message(embed=embedVar)
 
 @client.event
 async def on_ready():
